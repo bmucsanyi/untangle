31f8c4be5b4bfb172713d31b5789e0ebabb06c52
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2024-09-08 12:12:01 +0200

    Update SimpleConvNet

--- untangle/models/__init__.py
+++ untangle/models/__init__.py
@@ -1,7 +1,7 @@
 from .resnet_cifar import wide_resnet_c_28_10
 from .resnet_cifar_preact import resnet_c_preact_28, wide_resnet_c_preact_28_10
 from .resnet_imagenet import resnet50
-from .simple_convnet import SimpleConvNet
+from .simple_convnet import simple_convnet_3_256
 from .utils import (
     AvgPoolShortCut,
     BinaryClassifier,
@@ -14,9 +14,9 @@ __all__ = [
     "BinaryClassifier",
     "FlattenAdaptiveAvgPool2d",
     "NonNegativeRegressor",
-    "SimpleConvNet",
     "resnet50",
     "resnet_c_preact_28",
+    "simple_convnet_3_256",
     "wide_resnet_c_28_10",
     "wide_resnet_c_preact_28_10",
 ]
