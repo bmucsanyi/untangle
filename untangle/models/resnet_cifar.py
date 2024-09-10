a45209716643c680530a498caf53e05528c7792b
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2024-09-10 04:09:57 +0200

    Update ResNet CIFAR

--- untangle/models/resnet_cifar.py
+++ untangle/models/resnet_cifar.py
@@ -2,23 +2,23 @@
 
 from torch import nn
 
-from .utils import AvgPoolShortCut
+from .utils import FlattenAdaptiveAvgPool2d, PoolPad
 
 
-def wide_resnet_c_28_10(
+def wide_resnet_c_26_10(
     num_classes=10,
     in_chans=3,
-    down_type="conv",
+    downsample_type="conv",
     act_layer=nn.ReLU,
 ):
-    """Constructs a WideResNet-28-10 model."""
+    """Constructs a WideResNet-26-10 model."""
     model = ResNetC(
         block_fn=BasicBlockC,
-        depth=28,
+        depth=26,
         width_multiplier=10,
         num_classes=num_classes,
         in_chans=in_chans,
-        down_type=down_type,
+        downsample_type=downsample_type,
         act_layer=act_layer,
     )
 
@@ -30,7 +30,7 @@ class BasicBlockC(nn.Module):
 
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride, act_layer, down_type):
+    def __init__(self, in_planes, planes, stride, act_layer, downsample_type):
         super().__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
@@ -43,89 +43,33 @@ class BasicBlockC(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.act2 = act_layer(inplace=True)
 
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
-            if down_type == "conv":
-                self.shortcut = nn.Sequential(
+        self.downsample = nn.Identity()
+        out_planes = self.expansion * planes
+        if stride != 1 or in_planes != out_planes:
+            if downsample_type == "conv":
+                self.downsample = nn.Sequential(
                     nn.Conv2d(
-                        in_planes,
-                        self.expansion * planes,
+                        in_channels=in_planes,
+                        out_channels=out_planes,
                         kernel_size=1,
                         stride=stride,
                         bias=False,
                     ),
-                    nn.BatchNorm2d(self.expansion * planes),
-                )
-            elif down_type == "ddu":
-                self.shortcut = nn.Sequential(
-                    AvgPoolShortCut(
-                        stride=stride, out_c=self.expansion * planes, in_c=in_planes
-                    )
+                    nn.BatchNorm2d(out_planes),
                 )
             else:
-                msg = "Invalid downsample type provided."
-                raise ValueError(msg)
+                self.downsample = PoolPad(
+                    stride=stride,
+                    in_channels=in_planes,
+                    out_channels=out_planes,
+                    downsample_type=downsample_type,
+                )
 
     def forward(self, x):
         out = self.act1(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
+        out += self.downsample(x)
         out = self.act2(out)
-        return out
-
-
-class BottleneckC(nn.Module):
-    """Bottleneck module for CIFAR ResNets."""
-
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride, act_layer, down_type):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.act1 = act_layer(inplace=True)
-
-        self.conv2 = nn.Conv2d(
-            planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
-        )
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.act2 = act_layer(inplace=True)
-
-        self.conv3 = nn.Conv2d(
-            planes, self.expansion * planes, kernel_size=1, bias=False
-        )
-        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
-        self.act3 = act_layer(inplace=True)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
-            if down_type == "conv":
-                self.shortcut = nn.Sequential(
-                    nn.Conv2d(
-                        in_planes,
-                        self.expansion * planes,
-                        kernel_size=1,
-                        stride=stride,
-                        bias=False,
-                    ),
-                    nn.BatchNorm2d(self.expansion * planes),
-                )
-            elif down_type == "ddu":
-                self.shortcut = nn.Sequential(
-                    AvgPoolShortCut(
-                        stride=stride, out_c=self.expansion * planes, in_c=in_planes
-                    )
-                )
-            else:
-                msg = "Invalid downsample type provided"
-                raise ValueError(msg)
-
-    def forward(self, x):
-        out = self.act1(self.bn1(self.conv1(x)))
-        out = self.act2(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = self.act3(out)
         return out
 
 
@@ -139,20 +83,25 @@ class ResNetC(nn.Module):
         width_multiplier,
         num_classes,
         in_chans,
-        down_type,
+        downsample_type,
         act_layer,
     ):
         super().__init__()
         self.in_planes = 16
         self.num_classes = num_classes
         self.num_features = 64 * block_fn.expansion * width_multiplier
-        self.down_type = down_type
+        self.downsample_type = downsample_type
 
-        if (depth - 4) % 6 != 0:
-            msg = "Depth should be 6n+4 (e.g., 22, 34, 46, 58, 112, 1204)"
+        if (depth - 2) % 6 != 0:
+            msg = "Depth should be 6n+2 (e.g., 20, 32, 44, 56, 110, 1202)"
             raise ValueError(msg)
 
         n = (depth - 2) // 6
+
+        if downsample_type not in {"conv", "bed_of_nails_pad", "avg_pad"}:
+            msg = f"Invalid option '{downsample_type}' provided"
+            raise ValueError(msg)
+        self.downsample_type = downsample_type
 
         self.conv1 = nn.Conv2d(
             in_chans, 16, kernel_size=3, stride=1, padding=1, bias=False
@@ -168,7 +117,7 @@ class ResNetC(nn.Module):
         self.layer3 = self.make_layer(
             block_fn, 64 * width_multiplier, n, stride=2, act_layer=act_layer
         )
-        self.global_pool = nn.AvgPool2d(kernel_size=8)
+        self.global_pool = FlattenAdaptiveAvgPool2d()
         self.fc = nn.Linear(self.num_features, self.num_classes)
 
     def make_layer(self, block, planes, num_blocks, stride, act_layer):
@@ -178,7 +127,7 @@ class ResNetC(nn.Module):
                 planes=planes,
                 stride=stride,
                 act_layer=act_layer,
-                down_type=self.down_type,
+                downsample_type=self.downsample_type,
             )
         )
         self.in_planes = planes * block.expansion
@@ -190,7 +139,7 @@ class ResNetC(nn.Module):
                     planes=planes,
                     stride=1,
                     act_layer=act_layer,
-                    down_type=self.down_type,
+                    downsample_type=self.downsample_type,
                 )
             )
 
@@ -216,7 +165,6 @@ class ResNetC(nn.Module):
 
     def forward_head(self, x, *, pre_logits: bool = False):
         out = self.global_pool(x)
-        out = out.view(out.size(0), -1)
 
         return out if pre_logits else self.fc(out)
 
