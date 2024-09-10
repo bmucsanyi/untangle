d258378bde32bbe974b59ccafa1b5735cdbc2a45
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2024-09-10 04:11:43 +0200

    Add PoolPad

--- untangle/models/__init__.py
+++ untangle/models/__init__.py
@@ -1,20 +1,20 @@
-from .resnet_cifar import wide_resnet_c_28_10
-from .resnet_cifar_preact import resnet_c_preact_28, wide_resnet_c_preact_28_10
-from .resnet_imagenet import resnet50
+from .resnet_cifar import wide_resnet_c_26_10
+from .resnet_cifar_preact import resnet_c_preact_26, wide_resnet_c_preact_26_10
+from .resnet_imagenet import resnet_50
 from .utils import (
-    AvgPoolShortCut,
     BinaryClassifier,
     FlattenAdaptiveAvgPool2d,
     NonNegativeRegressor,
+    PoolPad,
 )
 
 __all__ = [
-    "AvgPoolShortCut",
     "BinaryClassifier",
     "FlattenAdaptiveAvgPool2d",
     "NonNegativeRegressor",
-    "resnet50",
-    "resnet_c_preact_28",
-    "wide_resnet_c_28_10",
-    "wide_resnet_c_preact_28_10",
+    "PoolPad",
+    "resnet_50",
+    "resnet_c_preact_26",
+    "wide_resnet_c_26_10",
+    "wide_resnet_c_preact_26_10",
 ]
