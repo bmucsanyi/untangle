2570ea99e63b8b9e86e553df3ad0d7da6dc06215
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2024-09-18 09:37:33 +0200

    Fix sigmoid NLL loss

--- untangle/losses/sigmoid_nll_loss.py
+++ untangle/losses/sigmoid_nll_loss.py
@@ -14,12 +14,13 @@ class SigmoidNLLLoss(nn.Module):
         self._softplus = nn.Softplus()
 
     def forward(self, logits, targets, *, apply_activation=True):
+        targets = F.one_hot(targets, num_classes=logits.shape[-1])
+
         if apply_activation:
             # Compute sigmoid BCE loss
             loss = self._softplus(logits) - targets * logits
         else:
             preds = logits
-            targets = F.one_hot(targets, num_classes=logits.shape[-1])
 
             # Compute loss
             loss = torch.where(targets == 1, -torch.log(preds), -torch.log(1 - preds))
