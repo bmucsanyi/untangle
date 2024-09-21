760c181adb9899f4b984bc3f3c41b46e1e40c9b4
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2024-09-21 20:10:25 +0200

    Update distributed.py

--- untangle/utils/distributed.py
+++ untangle/utils/distributed.py
@@ -6,39 +6,7 @@ import torch
 
 
 def is_distributed_env():
-    if "WORLD_SIZE" in os.environ:
-        return int(os.environ["WORLD_SIZE"]) > 1
-    if "SLURM_NTASKS" in os.environ:
-        return int(os.environ["SLURM_NTASKS"]) > 1
-
-    return False
-
-
-def world_info_from_env():
-    local_rank = 0
-    for v in (
-        "LOCAL_RANK",
-        "MPI_LOCALRANKID",
-        "SLURM_LOCALID",
-        "OMPI_COMM_WORLD_LOCAL_RANK",
-    ):
-        if v in os.environ:
-            local_rank = int(os.environ[v])
-            break
-
-    global_rank = 0
-    for v in ("RANK", "PMI_RANK", "SLURM_PROCID", "OMPI_COMM_WORLD_RANK"):
-        if v in os.environ:
-            global_rank = int(os.environ[v])
-            break
-
-    world_size = 1
-    for v in ("WORLD_SIZE", "PMI_SIZE", "SLURM_NTASKS", "OMPI_COMM_WORLD_SIZE"):
-        if v in os.environ:
-            world_size = int(os.environ[v])
-            break
-
-    return local_rank, global_rank, world_size
+    return int(os.environ.get("WORLD_SIZE"), "1") > 1
 
 
 def init_distributed_device(args):
@@ -50,32 +18,11 @@ def init_distributed_device(args):
     local_rank = 0
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    dist_backend = "nccl"
-    dist_url = "env://"
-
     if is_distributed_env():
-        if "SLURM_PROCID" in os.environ:
-            # DDP via SLURM
-            local_rank, global_rank, world_size = world_info_from_env()
-            # SLURM var -> torch.distributed vars if needed
-            os.environ["LOCAL_RANK"] = str(local_rank)
-            os.environ["RANK"] = str(global_rank)
-            os.environ["WORLD_SIZE"] = str(world_size)
-            torch.distributed.init_process_group(
-                backend=dist_backend,
-                init_method=dist_url,
-                world_size=world_size,
-                rank=global_rank,
-            )
-        else:
-            # DDP via torchrun
-            local_rank, _, _ = world_info_from_env()
-            torch.distributed.init_process_group(
-                backend=dist_backend,
-                init_method=dist_url,
-            )
-            world_size = torch.distributed.get_world_size()
-            global_rank = torch.distributed.get_rank()
+        local_rank = os.environ["LOCAL_RANK"]
+        torch.distributed.init_process_group(backend="nccl")
+        world_size = torch.distributed.get_world_size()
+        global_rank = torch.distributed.get_rank()
         distributed = True
 
     if distributed and device != "cpu":
